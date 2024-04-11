1428c861439f00b99155853e2202ab3933ffcb6e
Miguel Mazzoleni <127255940+miguel-mazzoleni@users.noreply.github.com>
2024-04-11 13:28:19 -0300

    Add files via upload

---
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,53 @@
+
+package com.mycompany.calculadora;
+import java.util.Scanner;
+/**
+ *
+ * @author migue
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Escolha a operação: ");
+        System.out.println("1 - Adição");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        int escolha = scanner.nextInt();
+
+        double resultado = 0;
+
+        switch (escolha) {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("Não é possível dividir por zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Escolha inválida.");
+                return;
+        }
+
+        System.out.println("O resultado é: " + resultado);
+    }
+}
